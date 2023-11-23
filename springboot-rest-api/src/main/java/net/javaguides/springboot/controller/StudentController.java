69a96f7dafad8afe79a682d64239c9ad67458111
pa-vani <pallapupavani96@gmail.com>
2023-11-23 16:39:40 +0530

    commit

--- springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -1,9 +1,15 @@
 package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.bean.Student;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -49,4 +55,22 @@ public class StudentController {
                                           @RequestParam String lastName){
         return new Student(id, firstName, lastName);
     }
+    
+     @PostMapping("students/create")
+     @ResponseStatus(HttpStatus.CREATED)
+     public Student createStudent(@RequestBody Student student) {
+    	 System.out.println(student.getId());
+    	 System.out.println(student.getFirstName());
+    	 System.out.println(student.getLastName());
+    	 return student;
+     }
+     
+     
+     @PutMapping("students/{id}/update")
+     public Student updateStudent(@RequestBody Student student,@PathVariable("id") int studentId) {
+    	 System.out.println(student.getFirstName());
+    	 System.out.println(student.getLastName());
+    	 return student;
+     }
+     
 }
