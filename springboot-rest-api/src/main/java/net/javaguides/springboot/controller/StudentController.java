7d10321e92595ab74eecd8495a526db27d45d7e4
pa-vani <pallapupavani96@gmail.com>
2023-11-23 15:59:07 +0530

    commit

---
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -0,0 +1,52 @@
+package net.javaguides.springboot.controller;
+
+import net.javaguides.springboot.bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    // http://localhost:8080/student
+    @GetMapping("student")
+    public Student getStudent(){
+        Student student = new Student(
+          1,
+          "Pavani",
+          "Pallapu"
+        );
+        return student;
+    }
+
+    
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Ramesh", "Fadatare"));
+        students.add(new Student(2, "Pavani", "Pallapu"));
+        students.add(new Student(3, "Ram", "Jadhav"));
+        students.add(new Student(4, "Sanjay", "Pawar"));
+        return students;
+    }
+
+    
+    @GetMapping("students/{id}/{first-name}/{last-name}")
+    public Student studentPathVariable(@PathVariable("id") int studentId,
+                                       @PathVariable("first-name") String firstName,
+                                       @PathVariable("last-name") String lastName){
+        return new Student(studentId, firstName, lastName);
+    }
+
+    
+    @GetMapping("students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName){
+        return new Student(id, firstName, lastName);
+    }
+}
